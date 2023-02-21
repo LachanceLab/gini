fc42f3137a5ab4a73a4d2b5031de0eb06e100a85
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2023-02-20 22:49:06 -0500

    added unadjusted and adjusted traitLD measurements

--- code_part1/calculate_traitLD.R
+++ code_part1/calculate_traitLD.R
@@ -13,6 +13,12 @@ dir_input_data <- "../input_data/"
 dir_ldscores <- paste0(dir_input_data, "ldscores/")
 
 pops <- c("EUR","AFR","AMR","CSA","EAS","MID")
+# set to TRUE if code has already been run once, meaning that the LD scores
+# have already been adjusted for AF (saves time)
+ldscores_already_ajdusted <- TRUE
+# set to TRUE if code has already been run once, meaning that the top independent
+# SNPs file already contains the ld-scores for each SNP
+top_SNPs_already_appended <- FALSE
 
 ## Functions
 
@@ -45,29 +51,35 @@ top_indep_SNPs <- as_tibble(fread(loc_top_indep_SNPs)) %>%
   filter(chrom != "X") %>%
   mutate(chrom = as.numeric(chrom))
 
-for (pop in pops) {
-  
-  loc_ldscores <- paste0(dir_input_data, "ldscores/UKBB.",pop,".ldscores_FULL_adj.txt")
-  
-  print(paste0(pop, ": Reading ldscores"))
-  ldscores <- as_tibble(fread(loc_ldscores))
-  print(paste0(pop, ": AF-adjusting ldscores"))
-  ldscores <- ldscores %>% adjust_LDscores()
-  
-  print(paste0(pop, ": Writing adjusted ldscores to system"))
-  fwrite(ldscores, loc_ldscores, sep="\t")
-  
-  print(paste0(pop, ": Joining ldscores to top_indep_SNPs"))
-  top_indep_SNPs <- top_indep_SNPs %>%
-    left_join(ldscores %>% select(CHR,BP, ld_score_adj), 
-              by=c("chrom"="CHR","chr_position"="BP"))
-  
-  colnames(top_indep_SNPs) <- c(colnames(top_indep_SNPs)[-ncol(top_indep_SNPs)],
-                                paste0("ld_score_adj_",pop))
-  
-}
 
-fwrite(top_indep_SNPs, loc_top_indep_SNPs, sep="\t")
+if (!top_SNPs_already_appended) {
+  for (pop in pops) {
+    
+    loc_ldscores <- paste0(dir_input_data, "ldscores/UKBB.",pop,".ldscores_FULL_adj.txt")
+    
+    print(paste0(pop, ": Reading ldscores"))
+    ldscores <- as_tibble(fread(loc_ldscores))
+    
+    if (!ldscores_already_ajdusted) {
+      print(paste0(pop, ": AF-adjusting ldscores"))
+      ldscores <- ldscores %>% adjust_LDscores()
+      
+      print(paste0(pop, ": Writing adjusted ldscores to system"))
+      fwrite(ldscores, loc_ldscores, sep="\t")
+    }
+    
+    print(paste0(pop, ": Joining ldscores to top_indep_SNPs"))
+    top_indep_SNPs <- top_indep_SNPs %>%
+      left_join(ldscores %>% select(CHR,BP, A0, A1, ld_score, ld_score_adj), 
+                by=c("chrom"="CHR","chr_position"="BP", "A1"="A0","A2"="A1"))
+    
+    colnames(top_indep_SNPs) <- c(colnames(top_indep_SNPs)[1:(ncol(top_indep_SNPs)-2)],
+                                  paste0("ld_score_",pop),paste0("ld_score_adj_",pop))
+    
+  }
+  
+  #fwrite(top_indep_SNPs, loc_top_indep_SNPs, sep="\t")
+}
 
 for (i in 1:nrow(traits_table)) {
   code <- traits_table$prive_code[i]
@@ -86,19 +98,31 @@ for (i in 1:nrow(traits_table)) {
     group_by(SNP) %>% filter(gvc == max(gvc)) %>% ungroup() %>%
     filter(chrom != "X") %>%
     mutate(chrom = as.numeric(chrom)) %>%
-    left_join(top_indep_SNPs_trait %>% select(chrom, chr_position, starts_with("ld_score_adj")),
-              by=c("chrom","chr_position")) %>%
+    left_join(top_indep_SNPs_trait %>%
+                select(chrom, chr_position, A1, A2, starts_with("ld_score_")),
+              by=c("chrom","chr_position","A1","A2")) %>%
     select(-starts_with(c("af_","pval_","beta_","se_", "low_confidence_")))
   
   for (pop in pops) {
-    col_ldscore <- paste0("ld_score_adj_",pop)
-    sf_top_pop <- sf_top %>%
-      select(gvc,ld_score_adj = !!as.name(col_ldscore)) %>% drop_na()
-    traitLD <- weighted.mean(sf_top_pop$ld_score_adj, sf_top_pop$gvc)
-    col_traitLD <- paste0("traitLD_",pop)
-    traits_table[i,col_traitLD] <- traitLD
-    print(paste(i, code, pop, round(traitLD,2)))
+    for (adj_status in c("","_adj")) {
+      col_ldscore <- paste0("ld_score",adj_status,"_",pop)
+      sf_top_pop <- sf_top %>%
+        select(gvc,ld_score = !!as.name(col_ldscore)) %>% drop_na()
+      traitLD <- weighted.mean(sf_top_pop$ld_score, sf_top_pop$gvc)
+      
+      if (adj_status == "") {
+        col_traitLD <- paste0("traitLD_unadj_",pop)
+      } else {
+        col_traitLD <- paste0("traitLD_adj_",pop)
+      }
+      
+      traits_table[i,col_traitLD] <- traitLD
+      print(paste(i, code, pop, col_traitLD, round(traitLD,2)))
+    }
   }
   
 }
 fwrite(traits_table, loc_traits_table, sep="\t")
+
+
+#traits_table <- traits_table %>% select(-starts_with("traitLD_"))
