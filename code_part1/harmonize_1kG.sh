cd52cc295953fb4cfde6fac81adb3cb9d7ffcc40
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2023-05-22 18:50:39 -0400

    1kG data now merges into a single file

--- code_part1/harmonize_1kG.sh
+++ code_part1/harmonize_1kG.sh
@@ -13,13 +13,18 @@ awk 'NR>1 {print $6}' full_variant_qc_metrics.txt > panUKB_SNPs_varIDs.txt
 
 # Downloads each 'integrated variant call set' file from 1kG phase3 (GRCh37) data
 # https://www.internationalgenome.org/data-portal/data-collection/phase-3
-for i in {1..22}; do
+# This loop takes over an hour to run
+for i in {9..22}; do
+  echo =======[ CHROMOSOME ${i} ]=======
+  
+  # Downloads 1kG vcf file and unzips it
   url="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
   file_gz="ALL.chr${i}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz"
   file=${file_gz:0:-3}
   wget ${url}
   gunzip -f ${file_gz}
   
+  # Uses plink2 to convert to bed, also filters to panUKBB SNPs
   ~/plink2/plink2 \
   --vcf ${file} \
   --extract panUKB_SNPs_varIDs.txt \
@@ -30,6 +35,21 @@ for i in {1..22}; do
   --keep-allele-order \
   --make-bed \
   --out 1kG_phase3_GRCh37_chr${i}
+  
+  # Removes original vcf file to save space
+  rm ${file}
+  
+  # Adds bfile prefix to list used later for merging by PLINK
+  echo 1kG_phase3_GRCh37_chr${i} >> list_1kG_phase3_GRCh37.txt
 done
 
+# Merges bfiles into one, should be about 25M SNPs
+~/plink1_9/plink \
+--merge-list list_1kG_phase3_GRCh37.txt \
+--make-bed \
+--out 1kG_phase3_GRCh37_ALL
 
+# Downloads 1kG sample file
+wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call_samples_v3.20130502.ALL.panel
+# Extracts just the FID, IID, and continent code
+awk 'BEGIN {OFS=" "} NR>1 {print "0", $1, $3}' integrated_call_samples_v3.20130502.ALL.panel > 1kG_continent_IDs.txt
