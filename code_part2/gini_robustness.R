79f5e1e94458315fe50f3982086f5dda073475d7
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2023-06-21 15:53:59 -0400

    added plot for gini and prop_gvc_top by pop,threshold

--- code_part2/gini_robustness.R
+++ code_part2/gini_robustness.R
@@ -1,9 +1,10 @@
 # gini_robustness.R
 # Calculates gini using different parameters to check robustness
 
-### Libraries and directories ####
+## Libraries and directories ####
 library(tidyverse)
 library(data.table)
+library(GGally)
 source("../code_part1/helper_functions/helper_functions.R")
 
 # sets working directory
@@ -19,6 +20,7 @@ traits_table <- as_tibble(fread(loc_table))
 qcodes <- (traits_table %>% filter(GWAS_trait_type=="quantitative",
                                    PGS_trait_type=="quantitative"))$prive_code
 
+## Computes Gini at different parameters ####
 # parameter options
 thresholds <- c(50, 100, 250, 500, 750, 1000)
 # makes empty table for collecting data
@@ -84,7 +86,67 @@ for (code in qcodes) {
     }
   }
 }
-
+robustness_tbl <- robustness_tbl %>%
+  mutate(prop_gvc_top = sum_gvc_top / sum_gvc_all)
 # saves results
 loc_out <- paste0("../generated_data/gini_robustness_data.txt")
-fwrite(robustness_tbl, loc_out, sep="\t")
+#fwrite(robustness_tbl, loc_out, sep="\t")
+
+## VISUALIZATION ####
+loc_out <- paste0("../generated_data/gini_robustness_data.txt")
+robustness_tbl <- as_tibble(fread(loc_out))
+
+### Printing function ####
+print_plot <- function(gg, loc_out, print_mode, plot_width, plot_height, sf) {
+  if (print_mode == "png") {
+    png(loc_out, width = plot_width*sf, height = plot_height*sf)
+  } else if (print_mode == "pdf") {
+    pdf(loc_out, width = plot_width*sf / 75, height = plot_height*sf / 75)
+  }
+  print(gg)
+  dev.off()
+}
+sf <- 2
+print_mode <- "png"
+# common theme for supplemental plots
+gini_p_theme <- theme(
+  plot.subtitle = element_text(size=20*sf),
+  axis.title = element_text(size=30*sf),
+  axis.text = element_text(size=22*sf),
+  legend.title = element_text(size=30*sf),
+  legend.text = element_text(size=25*sf),
+  plot.margin = unit(1*sf*c(1,1,1,1), "cm")
+)
+
+# color legend for plots
+gc_scale <- scale_color_manual(
+  labels = c("Biological Measures","Lifestyle/Psychological","Physical Measures"),
+  breaks = c("biological measures","lifestyle/psychological","physical measures"),
+  values = c("biological measures"="#F8766D","lifestyle/psychological"="#00BF7D","physical measures"="#00B0F6","psychological"="#E76BF3")
+)
+
+### Actual plots ####
+
+#### compares gini by population ####
+pop_threshold_tbl <- robustness_tbl %>% group_by(pop, threshold) %>%
+  summarize(gini = mean(gini),
+            prop_gvc_top = mean(prop_gvc_top)) %>%
+  filter(!pop %in% c("meta","meta_hq"))
+# plots gini by population and threshold
+ggplot(pop_threshold_tbl, aes(x= as.factor(threshold), y=gini)) +
+  geom_line(aes(color = pop, group = pop), size=1) +
+  xlab("Top # of SNPs used in Gini calculation") +
+  ylab("Gini") +
+  labs(title="Mean Gini for different top # of SNPs and populations",
+       color = "Population") +
+  theme_light()
+# plots prop_gvc_top by population and threshold
+ggplot(pop_threshold_tbl, aes(x=as.factor(threshold), y=prop_gvc_top)) +
+  geom_line(aes(color = pop, group = pop), size=1) +
+  xlab("Top # of SNPs used in Gini calculation") +
+  ylab("Proportion of total gvc captured within top SNPs") +
+  labs(title="Mean Proportion of gvc in top SNPs for different top # of SNPs and populations",
+       color = "Population") +
+  theme_light()
+
+
