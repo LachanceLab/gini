5029ecc0d766b7afd65e294b2b7d87c049eaed62
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2023-06-22 14:51:17 -0400

    now plots gini vs threshold, and not gini_rank

--- code_part2/gini_robustness.R
+++ code_part2/gini_robustness.R
@@ -5,8 +5,11 @@
 library(tidyverse)
 library(data.table)
 library(GGally)
+library(ggpubr)
 source("../code_part1/helper_functions/helper_functions.R")
 
+# Check whether robustness test should be redone or not
+run_robustness_loop <- FALSE
 # sets working directory
 setwd("./")
 
@@ -20,77 +23,79 @@ traits_table <- as_tibble(fread(loc_table))
 qcodes <- (traits_table %>% filter(GWAS_trait_type=="quantitative",
                                    PGS_trait_type=="quantitative"))$prive_code
 
-## Computes Gini at different parameters ####
-# parameter options
-thresholds <- c(50, 100, 250, 500, 750, 1000)
-# makes empty table for collecting data
-robustness_tbl <- tibble(
-  prive_code = as.character(),
-  pop = as.character(),
-  threshold = as.numeric(),
-  gini = as.numeric(),
-  n_sig_SNPs = as.numeric(),
-  n_sig_SNPs_pop = as.numeric(),
-  sum_gvc_top = as.numeric(),
-  sum_gvc_all = as.numeric()
-)
-
-ii <- 0
-# mega-loop
-for (code in qcodes) {
-  # reads summary file
-  #slice <- traits_table %>% filter(prive_code==code)
-  filename <- paste0(code,"_sf_indep.txt")
-  loc_summary_file <- paste0(dir_sf,filename)
-  sf <- as_tibble(fread(loc_summary_file))
-  # gets number of SNPs
-  n_sig_SNPs <- nrow(sf)
+if (run_robustness_loop) {
+  ## Computes Gini at different parameters ####
+  # parameter options
+  thresholds <- c(50, 100, 250, 500, 750, 1000)
+  # makes empty table for collecting data
+  robustness_tbl <- tibble(
+    prive_code = as.character(),
+    pop = as.character(),
+    threshold = as.numeric(),
+    gini = as.numeric(),
+    n_sig_SNPs = as.numeric(),
+    n_sig_SNPs_pop = as.numeric(),
+    sum_gvc_top = as.numeric(),
+    sum_gvc_all = as.numeric()
+  )
   
-  # loops through available gvc columns
-  cols_gvc <- colnames(sf %>% select(starts_with("gvc_")))
-  pops2use <- sapply(cols_gvc, function(x) substring(x, 5, nchar(x))) %>% unname()
-  for (pop in pops2use) {
-    col_gvc <- paste0("gvc_",pop)
-    sf_pop <- sf %>%
-      select(gvc_pop = !!enquo(col_gvc)) %>%
-      arrange(-gvc_pop) %>%
-      drop_na()
-    # gets number of SNPs and sum_gvc (for this population)
-    n_sig_SNPs_pop <- nrow(sf_pop)
-    sum_gvc_all <- sum(sf_pop$gvc_pop)
+  ii <- 0
+  # mega-loop
+  for (code in qcodes) {
+    # reads summary file
+    #slice <- traits_table %>% filter(prive_code==code)
+    filename <- paste0(code,"_sf_indep.txt")
+    loc_summary_file <- paste0(dir_sf,filename)
+    sf <- as_tibble(fread(loc_summary_file))
+    # gets number of SNPs
+    n_sig_SNPs <- nrow(sf)
     
-    for (threshold in thresholds) {
-      # gets top n SNPs, padding if necessary
-      gvc_list <- (sf_pop %>% filter(row_number() <= threshold))$gvc_pop %>%
-        pad_zeros(threshold)
+    # loops through available gvc columns
+    cols_gvc <- colnames(sf %>% select(starts_with("gvc_")))
+    pops2use <- sapply(cols_gvc, function(x) substring(x, 5, nchar(x))) %>% unname()
+    for (pop in pops2use) {
+      col_gvc <- paste0("gvc_",pop)
+      sf_pop <- sf %>%
+        select(gvc_pop = !!enquo(col_gvc)) %>%
+        arrange(-gvc_pop) %>%
+        drop_na()
+      # gets number of SNPs and sum_gvc (for this population)
+      n_sig_SNPs_pop <- nrow(sf_pop)
+      sum_gvc_all <- sum(sf_pop$gvc_pop)
       
-      # calculates gini
-      gini <- get_gini(gvc_list)
-      # gets the sum of gvc among top SNPs
-      sum_gvc_top <- sum(gvc_list)
-      
-      # records data
-      robustness_tbl <- robustness_tbl %>% add_row(
-        prive_code = code,
-        pop = pop,
-        threshold = threshold,
-        gini = gini,
-        n_sig_SNPs = n_sig_SNPs,
-        n_sig_SNPs_pop = n_sig_SNPs_pop,
-        sum_gvc_top = sum_gvc_top,
-        sum_gvc_all = sum_gvc_all
-      )
-      
-      print(paste(ii,code, pop, threshold, round(gini,3), sep="    "))
-      ii <- ii + 1
+      for (threshold in thresholds) {
+        # gets top n SNPs, padding if necessary
+        gvc_list <- (sf_pop %>% filter(row_number() <= threshold))$gvc_pop %>%
+          pad_zeros(threshold)
+        
+        # calculates gini
+        gini <- get_gini(gvc_list)
+        # gets the sum of gvc among top SNPs
+        sum_gvc_top <- sum(gvc_list)
+        
+        # records data
+        robustness_tbl <- robustness_tbl %>% add_row(
+          prive_code = code,
+          pop = pop,
+          threshold = threshold,
+          gini = gini,
+          n_sig_SNPs = n_sig_SNPs,
+          n_sig_SNPs_pop = n_sig_SNPs_pop,
+          sum_gvc_top = sum_gvc_top,
+          sum_gvc_all = sum_gvc_all
+        )
+        
+        print(paste(ii,code, pop, threshold, round(gini,3), sep="    "))
+        ii <- ii + 1
+      }
     }
   }
+  robustness_tbl <- robustness_tbl %>%
+    mutate(prop_gvc_top = sum_gvc_top / sum_gvc_all)
+  # saves results
+  loc_out <- paste0("../generated_data/gini_robustness_data.txt")
+  fwrite(robustness_tbl, loc_out, sep="\t")
 }
-robustness_tbl <- robustness_tbl %>%
-  mutate(prop_gvc_top = sum_gvc_top / sum_gvc_all)
-# saves results
-loc_out <- paste0("../generated_data/gini_robustness_data.txt")
-#fwrite(robustness_tbl, loc_out, sep="\t")
 
 ## VISUALIZATION ####
 loc_out <- paste0("../generated_data/gini_robustness_data.txt")
@@ -129,7 +134,7 @@ gc_scale <- scale_color_manual(
 
 #### compares gini by population ####
 pop_threshold_tbl <- robustness_tbl %>% group_by(pop, threshold) %>%
-  summarize(gini = mean(gini),
+  dplyr::summarize(gini = mean(gini),
             prop_gvc_top = mean(prop_gvc_top)) %>%
   filter(!pop %in% c("meta","meta_hq"))
 # plots gini by population and threshold
@@ -159,7 +164,7 @@ gg <- ggplot(prop_vs_gini_tbl, aes(x=prop_gvc_top, y=gini)) +
   coord_fixed() +
   labs(#title = expression(paste("Gini vs Proportion of ",italic(gvc)," among top SNPs for quantitative traits")),
     x = expression(paste("Proportion of total ", italic(gvc), " captured within top SNPs")),
-    y = expression(paste("Gini"[500])),
+    y = expression(paste("Gini"[list(500,Meta)])),
     color = "Trait Group") +
   theme_light() +
   gini_p_theme +
@@ -170,26 +175,26 @@ loc_out <- paste0("../generated_figures/gini_vs_prop-gvc-top.", print_mode)
 print(loc_out)
 print_plot(gg, loc_out, print_mode, 1200, 1200, sf)
 
-#### gini_rank at different thresholds ####
+#### gini at different thresholds ####
 gini_rank_tbl <- robustness_tbl %>%
   filter(pop == "meta2use") %>%
   group_by(threshold) %>%
   mutate(gini_rank = rank(gini)) %>%
   left_join(traits_table[c("prive_code","group_consolidated")], by="prive_code")
-gg <- ggplot(gini_rank_tbl, aes(x = as.factor(threshold), y = gini_rank)) +
+gg <- ggplot(gini_rank_tbl, aes(x = as.factor(threshold), y = gini)) +
   geom_line(aes(group=prive_code, color=group_consolidated), size=0.75*sf, key_glyph = "rect") +
-  labs(#title = 'Relative trait Gini rank at different top # of SNPs for 96 quantitative traits,
+  labs(#title = 'Gini vs top # of SNPs for 96 quantitative traits,
        x = 'Top # of SNPs used in Gini calculation',
-       y = 'Trait rank by Gini (low rank = high Gini)',
+       y = expression(paste("Gini"[list(500,Meta)])),
        color = "Trait Group") +
   scale_x_discrete(expand=c(0,0)) +
-  scale_y_continuous(expand=c(0.01,0.01)) +
-  scale_y_reverse() +
+  scale_y_continuous(expand=c(0.01,0.01), limits=c(0,1)) +
+  #scale_y_reverse() +
   theme_light() + gini_p_theme +
   theme(legend.position = "top") +
   gc_scale
 # prints out plot
-loc_out <- paste0("../generated_figures/gini-rank_vs_threshold.", print_mode)
+loc_out <- paste0("../generated_figures/gini_vs_threshold.", print_mode)
 print(loc_out)
 print_plot(gg, loc_out, print_mode, 1200, 1200, sf)
 
