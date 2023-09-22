4d322093b0c2bf8d65b8d38b1ff56c3457065af7
Nuno Carvalho <47678528+nunorgcarvalho@users.noreply.github.com>
2023-09-22 02:34:35 -0400

    combines gen. liabs. and generated phenotypes

---
+++ code_part3/compute_phenotype.R
@@ -0,0 +1,54 @@
+# 4 compute_phenotype.R
+
+# Libraries and paths ####
+library(tidyverse)
+library(data.table)
+#source("../code_part1/helper_functions/helper_functions.R")
+
+dir_sims <- "../generated_data/simulations/"
+
+# Compute genetic_liabilities ####
+dir_genliabs <- paste0(dir_sims,"genetic_liabilities/")
+
+# loads simmed phenotype table
+simphenos_tbl <- as_tibble(fread(paste0(dir_sims,"simphenos_tbl.txt")))
+
+# loops through each chromosome
+for (i in 1:22) {
+  print(i)
+  loc_gl <- paste0(dir_genliabs, "genetic_liabilities_chr",i,".sscore")
+  chr_gl <- as_tibble(fread(loc_gl))
+  
+  if (i==1) {
+    gl_matrix <- matrix(0, nrow=nrow(chr_gl), ncol=ncol(chr_gl)-2)
+  }
+  gl_matrix <- gl_matrix + as.matrix(chr_gl[1:nrow(chr_gl), 3:ncol(chr_gl)])
+}
+
+# makes genetic liabilities table
+GLs <- as_tibble(chr_gl %>% select(FID=`#FID`,IID) %>% cbind(gl_matrix))
+colnames(GLs)[-c(1:2)] <- paste0("gl_",simphenos_tbl$traitname)
+
+# saves genetic liabilities
+loc_out <- paste0(dir_sims, "genetic_liabilities.txt")
+fwrite(GLs, loc_out, sep="\t")
+
+
+# Compute phenotype ####
+pheno <- GLs
+for (i in 1:nrow(simphenos_tbl)) {
+  print(i)
+  # extracts trait genetic liabilities
+  gl <- GLs[[2+i]]
+  # generates environmental noise of variance ~= 1 - h2
+  env_noise <- rnorm(nrow(pheno), mean=0, sd=sqrt(1 - var(gl)))
+  
+  # centers phenotype and adds environmental noise
+  pheno[[2+i]] <- gl - mean(gl) + env_noise
+  # variance of phenotype should be ~1
+}
+# renames columns
+colnames(pheno)[-c(1:2)] <- paste0("ph_",simphenos_tbl$traitname)
+# saves phenotype data
+loc_out <- paste0(dir_sims, "pheno_values.txt")
+fwrite(pheno, loc_out, sep="\t")
